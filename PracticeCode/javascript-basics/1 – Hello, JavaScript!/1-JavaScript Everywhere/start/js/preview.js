8621a31a335e0f2a5106dc76b1e68bca366f6a01
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-13 11:16:50 -0700

    adding prompt for code to prompt user with a question

--- PracticeCode/javascript-basics/1 – Hello, JavaScript!/1-JavaScript Everywhere/start/js/preview.js
+++ PracticeCode/javascript-basics/1 – Hello, JavaScript!/1-JavaScript Everywhere/start/js/preview.js
@@ -1,4 +1,4 @@
-var name = "your name";
+var name = prompt("What is your name?");
 alert(`Hi, ${name}. Want to see something cool?`);
 document.querySelector('html').innerHTML = `
   <h1>Welcome to ${name}'s site!</h1>
