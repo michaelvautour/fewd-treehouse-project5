ac9124171ad02633afefa685366d5891ea4532e6
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-13 10:17:54 -0700

    changing game variables to load new pixel art images

--- PracticeCode/introducing_javascript/04_adding_graphics/game.js
+++ PracticeCode/introducing_javascript/04_adding_graphics/game.js
@@ -36,9 +36,9 @@ function addPlatforms() {
   platforms.create(50, 300, 'platform');
   platforms.create(150, 250, 'platform');
   platforms.create(650, 300, 'platform');
-  platforms.create(550, 200, 'platform');
-  platforms.create(300, 450, 'platform');
-  platforms.create(400, 350, 'platform');
+  platforms.create(550, 200, 'platform2');
+  platforms.create(300, 450, 'platform2');
+  platforms.create(400, 350, 'platform2');
   platforms.setAll('body.immovable', true);
 }
 
@@ -81,7 +81,7 @@ window.onload = function () {
     game.stage.backgroundColor = '#5db1ad';
     
     //Load images
-    game.load.image('platform', 'platform_1.png');
+    game.load.image('platform2', 'platform_2.png');
     
     //Load spritesheets
     game.load.spritesheet('player', 'chalkers.png', 48, 62);
