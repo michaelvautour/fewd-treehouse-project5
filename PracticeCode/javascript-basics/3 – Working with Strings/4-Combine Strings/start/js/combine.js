f5503dac125a8997ff5e074679d45f8257b6f472
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-13 16:55:10 -0700

    string combinations and uses of const vs lets for variable storing

---
+++ PracticeCode/javascript-basics/3 – Working with Strings/4-Combine Strings/start/js/combine.js
@@ -0,0 +1,4 @@
+const uName =prompt("What is your name?");
+let message = "Hello " + uName + ". Welcome to my music site. ";
+message += "I'm so happy that you came back to visit.";
+console.log(message);
