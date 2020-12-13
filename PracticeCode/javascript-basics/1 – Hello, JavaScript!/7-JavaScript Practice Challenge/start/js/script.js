99292062f41d44a921029a4872d51780a79568ec
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-13 12:12:15 -0700

    re-arranged some of the document data

--- PracticeCode/javascript-basics/1 – Hello, JavaScript!/7-JavaScript Practice Challenge/start/js/script.js
+++ PracticeCode/javascript-basics/1 – Hello, JavaScript!/7-JavaScript Practice Challenge/start/js/script.js
@@ -2,12 +2,12 @@
 alert("Warning! This message will self-destruct in");
 
 // 2. Display a "3... 2... 1..." countdown using 3 alert dialog boxes
-alert("3");
-alert("2");
-alert("1");
+alert("3...");
+alert("2...");
+alert("1...");
 
 // 3. This statement selects the <h1> element and replaces its text with "BOOM!".
-document.querySelector("h1").textContent = "🔥BOOM!🔥";
+document.querySelector("h1").textContent = "🔥 BOOM!🔥";
 
 // 4. Log "Message destroyed!" to the console
 console.log("Message destroyed");
