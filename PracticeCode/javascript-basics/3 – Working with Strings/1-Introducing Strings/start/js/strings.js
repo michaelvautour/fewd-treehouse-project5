95ab385b39265211e8becadc233ac938ecb6acff
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-13 13:45:28 -0700

    methods and length functions for string manipulation

--- PracticeCode/javascript-basics/3 – Working with Strings/1-Introducing Strings/start/js/strings.js
+++ PracticeCode/javascript-basics/3 – Working with Strings/1-Introducing Strings/start/js/strings.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+
+// let message = prompt("What do you say?");
+// console.log(message);
+
+// console.log("Hello, world!");
+
+// Not a method because no () after length
+// const passphrase = "hi";
+// console.log(passphrase.length);
+
+// Method because toUpperCase has a () on the end
+const passphrase = "hi";
+console.log(passphrase.toUpperCase());
