e342b4748735ec2f46b443ab8b828603a76041af
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-13 17:34:14 -0700

    insert <p> tags with variable names into html document

--- PracticeCode/javascript-basics/3 – Working with Strings/7-Variables and Strings Challenge/start/js/story.js
+++ PracticeCode/javascript-basics/3 – Working with Strings/7-Variables and Strings Challenge/start/js/story.js
@@ -1,7 +1,10 @@
 // 1. Declare variables and capture input.
-
+const quest1 = prompt("What is your name?");
+const quest2 = prompt("Where were you born?");
+const quest3 = prompt("What month were you born?");
 
 // 2. Combine the input with other words to create a story.
-
+const story = `<p>${quest1}, you have been given a special mission. You must go to ${quest2} in ${quest3} to save the world!<p>`
 
 // 3. Display the story as a <p> inside the <main> element.
+document.querySelector(`main`).innerHTML = story;
