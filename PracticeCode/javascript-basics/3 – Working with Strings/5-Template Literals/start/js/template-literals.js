37df5896d1ff6b98da9d9f76535f5c3d9734229b
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-13 17:07:04 -0700

    template-literals ' ${3*6} '

--- PracticeCode/javascript-basics/3 – Working with Strings/5-Template Literals/start/js/template-literals.js
+++ PracticeCode/javascript-basics/3 – Working with Strings/5-Template Literals/start/js/template-literals.js
@@ -1,5 +1,6 @@
-const name = prompt('What is your name?');
+const name = prompt(`What is your name?`);
 
-const message = "Hello" + name;
+const message = `Hello, ${name}. Welcome to my music site. I'm happy that you came
+by to visit, ${name}. Please come again and listen to more music!`;
 
 console.log(message);
