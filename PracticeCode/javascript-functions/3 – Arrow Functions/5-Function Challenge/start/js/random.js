464573dcfdb0d2736a3183f5fd7f11ff7255d2db
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-19 14:38:50 -0700

    practiced methods for validating that arguments passed to my functions are valid and prompted error if unexpected values are being passed

--- PracticeCode/javascript-functions/3 – Arrow Functions/5-Function Challenge/start/js/random.js
+++ PracticeCode/javascript-functions/3 – Arrow Functions/5-Function Challenge/start/js/random.js
@@ -5,9 +5,21 @@
  * @param {number} upper - The highest number value.
  * @return {number} The random number value.
  */
-
-Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+function callRandomNum(startNum, endNum) {
+    if ( isNaN(startNum) || isNaN(endNum) ) {
+        throw Error ('You need to enter a number');
+    } else {
+        const randNum = Math.floor(Math.random() * (startNum - endNum + 1)) + endNum;
+        return randNum;
+    }
+}
 
 // Call the function and pass it different values
 
-
+console.log(callRandomNum(100, 1000));
+console.log(callRandomNum(33, 330));
+console.log(callRandomNum('fudge', 10));
+console.log(callRandomNum(1, 10));
+console.log(callRandomNum(5, 'hello'));
+console.log(callRandomNum(10, 109));
+console.log(callRandomNum(22, 54));
