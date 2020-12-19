df4e5490b1367690ed5e3b27af3fd14408f2e403
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-19 14:56:29 -0700

    while loop with randomnumberguess game

--- PracticeCode/javascript-loops/1 - Simplify Repetitive Tasks with Loops/2 - The while Loop/start/js/while.js
+++ PracticeCode/javascript-loops/1 - Simplify Repetitive Tasks with Loops/2 - The while Loop/start/js/while.js
@@ -1,3 +1,10 @@
 function getRandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
+
+let counter = 0;
+
+while (counter < 100) {
+  console.log(`The random # is: ${getRandomNumber(10)}`);
+  counter += 1;
+}
