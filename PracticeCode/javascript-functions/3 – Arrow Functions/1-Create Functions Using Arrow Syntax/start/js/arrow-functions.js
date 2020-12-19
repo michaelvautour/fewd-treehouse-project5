990377dcd5137aa34a7478f8ec78114e97fc0a09
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-19 12:52:32 -0700

    converting declaration functions to arrow expressions

--- PracticeCode/javascript-functions/3 – Arrow Functions/1-Create Functions Using Arrow Syntax/start/js/arrow-functions.js
+++ PracticeCode/javascript-functions/3 – Arrow Functions/1-Create Functions Using Arrow Syntax/start/js/arrow-functions.js
@@ -1,4 +1,27 @@
-function getRandomNumber() {
-  const randomNumber = Math.floor( Math.random() * 6 ) + 1;
-  return randomNumber;
-}
+// function getRandomNumber() {
+//   const randomNumber = Math.floor( Math.random() * 6 ) + 1;
+//   return randomNumber;
+// }
+
+// converting above to a arrow function:
+const getRandomNumber = (upper) => {
+    const randomNumber = Math.floor( Math.random() * upper ) + 1;
+    return randomNumber;
+  }
+
+  //Acts similar to expressions for arrow expressions because it needs to be defined
+  // above the code before it's called, unlike declaration functions
+
+//write below as an arrow function
+//BEFORE
+  function getArea(width, length, unit) {
+    const area = width * length;
+    return `${area} ${unit}`;
+  }
+
+  //Here is the re-written code as an arrow, changed first variable name to showcase better
+  //AFTER
+  const getArea2 = (width, length, unit) => {
+    const area = width * length;
+    return `${area} ${unit}`;
+  }
