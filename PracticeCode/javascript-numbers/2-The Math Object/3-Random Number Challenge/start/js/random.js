7bda50336beb3d6bc86733118937f16b15136a27
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-18 20:52:58 -0700

    if else with random generator to prevent NaN when non-# values are given

--- PracticeCode/javascript-numbers/2-The Math Object/3-Random Number Challenge/start/js/random.js
+++ PracticeCode/javascript-numbers/2-The Math Object/3-Random Number Challenge/start/js/random.js
@@ -7,11 +7,14 @@ const userInput = prompt("Put a number in here");
 // Convert the input to a number
 let numberInput = parseInt(userInput);
 
-// Use Math.random() and the user's number to generate a random number
+if (numberInput) {
+    // Use Math.random() and the user's number to generate a random number
+    let randomInput = Math.floor(Math.random() * numberInput) +1;
 
-let randomInput = Math.floor(Math.random() * numberInput) +1;
+    // Create a message displaying the random number
+    console.log(`${randomInput} is a random number between 1 and ${numberInput}`);
+    alert(`${randomInput} is a random number between 1 and ${numberInput}`);
 
-// Create a message displaying the random number
-
-console.log(`${randomInput} is a random number between 1 and ${numberInput}`);
-alert(`${randomInput} is a random number between 1 and ${numberInput}`);
+} else {
+    console.log(`${userInput} is not a number, try again`);
+}
