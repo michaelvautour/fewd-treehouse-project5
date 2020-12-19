e10fd676ae9d066e5e254d4b75d02dd25d1bda2f
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-18 20:37:33 -0700

    created # randomizer that sets the users value as the highest value input and provides a random # between 1 and X = user input

--- PracticeCode/javascript-numbers/2-The Math Object/3-Random Number Challenge/start/js/random.js
+++ PracticeCode/javascript-numbers/2-The Math Object/3-Random Number Challenge/start/js/random.js
@@ -1,11 +1,17 @@
-// Collect input from a user
+//Should display randomly from 1 to the users entered #
 
+
+// Collect input from a user
+const userInput = prompt("Put a number in here");
 
 // Convert the input to a number
-
+let numberInput = parseInt(userInput);
 
 // Use Math.random() and the user's number to generate a random number
 
+let randomInput = Math.floor(Math.random() * numberInput) +1;
 
 // Create a message displaying the random number
 
+console.log(`${randomInput} is a random number between 1 and ${numberInput}`);
+alert(`${randomInput} is a random number between 1 and ${numberInput}`);
