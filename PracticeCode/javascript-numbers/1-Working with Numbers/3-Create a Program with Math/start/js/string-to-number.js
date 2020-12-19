009bf815316b09de0a9b729e837d09e1441e3af2
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-18 20:11:04 -0700

    string to # conversion using parseInt() and parseFloat() as well as + in front of variable

--- PracticeCode/javascript-numbers/1-Working with Numbers/3-Create a Program with Math/start/js/string-to-number.js
+++ PracticeCode/javascript-numbers/1-Working with Numbers/3-Create a Program with Math/start/js/string-to-number.js
@@ -1,3 +1,8 @@
 const HTMLBadges = prompt('How many HTML badges do you have?');
 const CSSBadges = prompt('How many CSS badges do you have?');
 
+const totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges);
+
+const pi = '3.14';
+
+console.log(parseFloat(pi));
