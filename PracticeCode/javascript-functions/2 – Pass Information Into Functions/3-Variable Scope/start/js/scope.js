c1f555f3401fc28e9590d9bc77789246af280fe0
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-19 12:12:00 -0700

    defining differences on global vs local scope variables

--- PracticeCode/javascript-functions/2 – Pass Information Into Functions/3-Variable Scope/start/js/scope.js
+++ PracticeCode/javascript-functions/2 – Pass Information Into Functions/3-Variable Scope/start/js/scope.js
@@ -1,4 +1,10 @@
+//Global scope variables
+let person = 'Lee';
+
 function greeting() {
+//function variables, if I remove the 'Let' it could access the global function but NOT recommended
   let person = 'Meg';
   alert(`Hi, ${person}!`);
 }
+
+// 
