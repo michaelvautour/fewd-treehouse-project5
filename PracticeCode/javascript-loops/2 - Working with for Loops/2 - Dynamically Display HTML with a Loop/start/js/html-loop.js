083487deb9885c81ea732017467e2a93e7a19be5
Michael Vautour <michael.e.vautour@gmail.com>
2020-12-20 12:57:21 -0700

    break out of a loop example

--- PracticeCode/javascript-loops/2 - Working with for Loops/2 - Dynamically Display HTML with a Loop/start/js/html-loop.js
+++ PracticeCode/javascript-loops/2 - Working with for Loops/2 - Dynamically Display HTML with a Loop/start/js/html-loop.js
@@ -7,3 +7,18 @@ for (let i = 5; i <= 100; i+=5) {
 }
 
 main.innerHTML = html;
+
+
+//Code to break out of a loop example:
+let message = "supercalifragilisticexpialidocious";
+message = message.length;
+
+for ( let i = 1; i < message; i++ ) {
+  if ( i === message / 2 ) {
+    console.log('The loop has terminated...');
+    break;
+  } {
+  console.log(`Logging the number ${i}`);
+}
+}
+console.log('The program continues...');
